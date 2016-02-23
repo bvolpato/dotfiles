605fd95f006a8cc68a00c127ebe02d4ed847e915
Bruno Volpato <brunocvcunha@gmail.com>
2016-02-22 23:48:27 -0800

    brew install & packages

---
+++ brew.sh
@@ -0,0 +1,10 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap caskroom/cask
+brew install brew-cask
+brew tap caskroom/versions
+
+brew install node
+brew install git
+brew install wget
+
+
