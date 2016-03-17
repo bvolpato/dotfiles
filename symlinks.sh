bb0f4ee4ef1d260e668bbc1a7583caf97353c626
Bruno Volpato <brunocvcunha@gmail.com>
2016-03-17 15:58:46 -0400

    prevent bower from asking questions

--- symlinks.sh
+++ symlinks.sh
@@ -2,11 +2,14 @@
 
 ln -s ~/githubworkspace/dotfiles/.bashrc ~/.bashrc
 ln -s ~/githubworkspace/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/githubworkspace/dotfiles/.wgetrc ~/.wgetrc
-ln -s ~/githubworkspace/dotfiles/.curlrc ~/.curlrc
-ln -s ~/githubworkspace/dotfiles/.inputrc ~/.inputrc
-ln -s ~/githubworkspace/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/githubworkspace/dotfiles/.osx ~/.osx
+
+ln -sf ~/githubworkspace/dotfiles/.bowerrc ~/.bowerrc
+ln -sf ~/githubworkspace/dotfiles/.wgetrc ~/.wgetrc
+ln -sf ~/githubworkspace/dotfiles/.curlrc ~/.curlrc
+ln -sf ~/githubworkspace/dotfiles/.inputrc ~/.inputrc
+ln -sf ~/githubworkspace/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/githubworkspace/dotfiles/.osx ~/.osx
+ln -sf ~/githubworkspace/dotfiles/.gitignore_global ~/.gitignore_global
+
 ln -s ~/githubworkspace/dotfiles/.vimrc ~/.vimrc
 ln -s ~/githubworkspace/dotfiles/.vim/ ~/
-ln -s ~/githubworkspace/dotfiles/.gitignore_global ~/.gitignore_global
