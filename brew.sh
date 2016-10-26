7b4b8f3f4495517a660db026c45a2220293fcb5d
Bruno Volpato <brunocvcunha@gmail.com>
2016-10-26 16:21:07 -0700

    launchcontrol

--- brew.sh
+++ brew.sh
@@ -312,6 +312,7 @@ if [ "$(uname -s)" == "Darwin" ]; then
   brew cask install --appdir="/Applications" atom
   brew cask install --appdir="/Applications" virtualbox
   brew cask install --appdir="/Applications" vagrant
+  brew cask install --appdir="/Applications" launchcontrol
   #brew cask install --appdir="/Applications" macdown
   #brew cask install --appdir="/Applications" mactex
 
